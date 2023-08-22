c9417d20993cbdd637c610332fb19ec073371c04
Nemanja Mucalovic <nemanjamucalovic@gmail.com>
2023-08-22 16:06:14 +0200

    added date param,csv download,fix data format

--- get_data.py
+++ get_data.py
@@ -1,13 +1,39 @@
 import googlemaps
 import os
+from datetime import datetime
+import csv
+import uuid
 
 
 api_key = os.environ.get('MAPS_API_KEY')
 gmaps = googlemaps.Client(key=api_key)
 
+tags_dict = {"religious_tourism":["church","sinagoge","hindu_temple", "tourist_attraction", "mosque"],
+             "animal_sighting":["zoo","aquarium"]}
+
 def convert_location_to_geocode(location):
     result = gmaps.geocode(location)
     return result[0]['geometry']['location']
+
+def write_data_to_csv(data_list):
+    output_file = f'{str(uuid.uuid4())}.csv'
+    if not data_list:
+        print("Data list is empty. Nothing to write to CSV.")
+        return
+    field_names = data_list[0].keys()
+    with open(output_file, "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
+
+        # Write the header row
+        writer.writeheader()
+
+        # Write the data rows
+        for row in data_list:
+            writer.writerow(row)
+
+    print(f"Data successfully written to {output_file}")
+    return output_file[:-4]
+
 
 def get_places(location, place_type):
     geocoded_location = convert_location_to_geocode(location)
@@ -22,53 +48,53 @@ def get_places(location, place_type):
                 "lng": place["geometry"]["location"].get("lng","N/A"),
                 "name": place.get("name"),
                 "user_ratings_total" : place.get("user_ratings_total","N/A"),
-                "rating": place.get("rating")
+                "rating": place.get("rating"),
+                "place_id": place.get("place_id"),
+                "place_type": place.get("place_type")
              }
-
         places.append(place_info)
+    print(f"number of all places {len(places)}")
     return places
 
-def get_locations(location, place_type):
+def get_working_hours(place_id, date):
+    place_details = gmaps.place(place_id)
+    opening_hours_data = place_details["result"].get("opening_hours", {}).get("weekday_text", [])
+    date_obj = datetime.strptime(date, "%Y-%m-%d")
+    date_to_check = datetime.date(date_obj)
+    day_of_week = date_to_check.weekday()
+
+    if opening_hours_data:
+        opening_hours_for_day = opening_hours_data[day_of_week]
+        print(opening_hours_for_day + place_details["result"]["name"])
+        return "Closed" not in opening_hours_for_day
+    else:
+        return False
+
+
+def get_directions(location, place_type, date):
     list_of_places = get_places(location, place_type)
-    return [
+    places_temp = [
         place
         for place in list_of_places
         if place['business_status'] == 'OPERATIONAL'
         and place['rating'] is not None
         and place['user_ratings_total'] > 10
     ]
-
-def get_directions(location, place_type):
-        list_of_places = get_places(location, place_type)
-        filtered_places = []
-        for place in list_of_places:
-            if place['business_status'] == 'OPERATIONAL' and place['rating'] is not None and place['user_ratings_total'] > 10:
-                filtered_places.append(place)
-                #print(filtered_places)
-                visits = [(place["lat"], place["lng"]) for place in filtered_places]
-                waypoints = "|".join([f"{lat},{lng}" for lat, lng in visits])
-        print(filtered_places)
-        directions_result = gmaps.directions(
-                    origin=location,
-                    destination=location,
-                    waypoints=waypoints,
-                    mode="walking",
-                    optimize_waypoints=True
-                    )
-        if directions_result:
-            best_route = directions_result[0]
-            return best_route
-        else:
-            return None
-    #filtiraj po ratingu,da li otvoren ili zatvoren,sta ima od tagova
-    #ubaciti rating,
-    #od svih lokacija koje imaju rating,izracunaj srednji i sve ispod toga izbaci
-    #isto za broj user ratinga
-    #print (places)
-    #visits = [(place["geometry"]["location"]["lat"], place["geometry"]["location"]["lng"])
-                 #for place in places]
-
-
-
-
-#get_places('Novi Sad','museum')
+    print(f"number of places that are filtered {len(places_temp)}")
+    filtered_places = [
+        place for place in places_temp if get_working_hours(place['place_id'], date)
+    ]
+    print(f"number of places to display {len(filtered_places)}")
+    name_of_csv = {"csv_id":write_data_to_csv(filtered_places)}
+    visits = [(place["lat"], place["lng"]) for place in filtered_places]
+    waypoints = "|".join([f"{lat},{lng}" for lat, lng in visits])
+    if directions_result := gmaps.directions(
+        origin=location,
+        destination=location,
+        waypoints=waypoints,
+        mode="walking",
+        optimize_waypoints=True,
+    ):
+        return [filtered_places,directions_result[0], name_of_csv]
+    else:
+        return None
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Body
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
-from get_data import get_places, get_directions, get_locations
+from get_data import get_places, get_directions
 from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
@@ -15,18 +17,21 @@ app.add_middleware(
 class Direction(BaseModel):
     location: str
     place_type: str
+    date: str
+
 
 @app.get("/health")
-def health_check():
+async def health_check():
     return {"status": "alive"}
 
-
 @app.post("/direction")
-def post_data(direction: Direction):
-    return get_directions(location=direction.location, place_type=direction.place_type)
+async def post_data(direction: Direction):
+    return get_directions(location=direction.location, place_type=direction.place_type, date=direction.date)
 
-@app.post("/locations")
-def post_locations(direction: Direction):
-    return get_locations(location=direction.location, place_type=direction.place_type)
+@app.get("/downloads/{csv_id}", response_class=FileResponse)
+async def get_csv(csv_id: str):
+    return f'{csv_id}.csv'
+
+
 
 
